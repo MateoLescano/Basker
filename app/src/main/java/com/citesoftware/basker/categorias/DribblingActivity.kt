f7d68d07fd019e41cd4d392762c6e488940d0f91
MateoLescano <85969608+MateoLescano@users.noreply.github.com>
2021-07-18 19:24:41 -0300

    18-7

--- app/src/main/java/com/citesoftware/basker/categorias/DribblingActivity.kt
+++ app/src/main/java/com/citesoftware/basker/categorias/DribblingActivity.kt
@@ -23,21 +23,21 @@ class DribblingActivity : AppCompatActivity() {
         when(intent.getIntExtra("dia", 1)){
 
             1 -> {
-                exercisesList.add(DataModel("Dribble en V Frontal","Con los pies abiertos al ancho de los hombros, las rodillas flexionadas y mirando al frente, dribbleo de lado a lado frontalmente. Aumento la velocidad Y fuerza tanto como pueda.", "3 series de 20 dribbles por cada mano." , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("8's","Con postura baja, dribbleo lo más cerca al piso que pueda en forma de 8 entre las piernas. Aumento la velocidad Y fuerza tanto como pueda." , "10 vueltas completas en una dirrección y 10 más en la otra." , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("Dribble con sprint", "De punta a punta de la cancha, dribbleo en velocidad, freno, cambio la mano del dribble, y vuelvo a acelerar. Nunca paro de dribblear y aumento la velocidad tanto como pueda." , "4 series de punta a punta de la cancha." , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("Dribble entre conos", "Coloco conos/objetos en una linea recta. Dribbleo entre los conos (como en un examen de manejo) en zigzag, luego cambiando de manos, luego entre las piernas, y finalmente por detras de la espalda." , "2 series de punta a punta" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex10),getString(R.string.Dex10), getString(R.string.Rex10) , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex11),getString(R.string.Dex11) , getString(R.string.Rex11) , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex12), getString(R.string.Dex12) , getString(R.string.Rex12) , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex13), getString(R.string.Dex13) , getString(R.string.Rex13) , R.drawable.placeholdergif))
 
             }
             2 -> {
-                exercisesList.add(DataModel("Dribble en V Lateral","Con los pies abiertos al ancho de los hombros, las rodillas flexionadas y mirando al frente, dribbleo de adelante hacía atrás lateralmente. Aumento la velocidad Y fuerza tanto como pueda.", "3 series de 20 dribbles por cada mano." , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("Dribble entre conos dobles", "Coloco dos filas de conos/objetos en linea recta. Dribbleo entre los conos de una fila a la otra en zigzag, cambiando de manos, luego entre las piernas, y finalmente por detras de la espalda." , "2 series de punta a punta" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("Dribble en cruz", "Con los pies abiertos al ancho de los hombros, las rodillas flexionadas y mirando al frente, dribbleo de izquierda a derecha en el frente, entre las piernas, de izquierda a derecha por la espalda, y nuevamente entre las piernas.", "10 series en una dirección y 10 en la otra." , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex14),getString(R.string.Dex14), getString(R.string.Rex14) , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex15), getString(R.string.Dex15) , getString(R.string.Rex15) , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex16), getString(R.string.Dex16), getString(R.string.Rex16) , R.drawable.placeholdergif))
                 exercisesList.add(DataModel(getString(R.string.TFT), getString(R.string.DFT), getString(R.string.RFT1) , R.drawable.placeholdergif))
 
             }
             3 -> {
-                exercisesList.add(DataModel("Dribbling estacionario", "Con los pies abiertos al ancho de los hombros, las rodillas flexionadas y mirando al frente, pico la pelota verticalmente aumentando la velocidad y la fuerza tanto como pueda." , "3 series de 60 segundos por mano." , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex17), getString(R.string.Dex17) , getString(R.string.Rex17) , R.drawable.placeholdergif))
 
             }
         }
