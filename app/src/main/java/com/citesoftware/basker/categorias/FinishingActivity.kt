52207d7f20adb0f0ff3a23fc9c9f5ab67c7930fb
MateoLescano <85969608+MateoLescano@users.noreply.github.com>
2021-07-24 15:27:22 -0300

    24-7

--- app/src/main/java/com/citesoftware/basker/categorias/FinishingActivity.kt
+++ app/src/main/java/com/citesoftware/basker/categorias/FinishingActivity.kt
@@ -25,19 +25,16 @@ class FinishingActivity : AppCompatActivity() {
             1 -> {
                 exercisesList.add(DataModel("Bandeja ambidiestra","Hago bandejas comunes en diagonal al aro desde ambos lados, concentrandome en los pasos previos y su orden (primero pie derecho y luego izquierdo para bandeja de mano derecha y viceversa para izquierda).", "15 bandejas por lado" , R.drawable.placeholdergif))
                 exercisesList.add(DataModel("Bandejas bajo el aro","Me paro debajo del aro, mirando hacía el otro aro. Sin mover los pies, hago bandejas con tablero, sin tablero", "10 bandejas de cada tipo" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("Gancho fijo","Me paro enfrente del aro, en paralelo al tablero. Tiro un gancho con el brazo más alejado del aro concentrandome en hacer un giro amplio", "12 ganchos por lado" , R.drawable.placeholdergif))
                 exercisesList.add(DataModel("Bandeja reversa","Empiezo desde fuera de la pintura en paralelo al tablero, y defino con una bandeja pasada de reversa", "12 por lado" , R.drawable.placeholdergif))
             }
             2 -> {
                 exercisesList.add(DataModel("Mikan Drill","Desde abajo del aro, doy un paso con la pierna izquierda hacía la derecha y en el mismo movimiento con hago una bandeja de mano derecha. Agarro la pelota apenas puedo y hago el movimiento inverso hacía la izquierda, y así sucesivamente.", "3 series de 10 bandejas seguidas" , R.drawable.placeholdergif))
                 exercisesList.add(DataModel("Stop and go","Empiezo dribleando desde fuera de la pintura. Entro con velocidad, me detengo sin dejar de driblear y ataco el aro con bandeja normal, eurostep o flotadora.", "2 series de 15" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("Gancho simple","Entro a la pintura en paralelo al tablero y tiro un gancho. Me concentro en dar los pasos correctos y en que la pelota no baje de la altura del hombro.", "12 ganchos por lado" , R.drawable.placeholdergif))
                 exercisesList.add(DataModel("Euro Step","Empiezo dribleando desde fuera de la pintura  y hago un eurostep. Hago eurostep para maximizar la distancia (manteniendo el lado del aro que estoy atacando) y para cambiar de dirección en el segundo paso (para tirar de frente o con bandeja reversa)", "15 euro steps por lado" , R.drawable.placeholdergif))
             }
             3 -> {
                 exercisesList.add(DataModel("Bandeja con finta","Hago bandejas comunes en diagonal al aro desde ambos lados, pero mientras doy el primer paso hago una finta (amague de pase, movimiento circular, cambio de mano por detras de la espalda)", "15 bandejas por lado" , R.drawable.placeholdergif))
                 exercisesList.add(DataModel("Hop step","Empiezo dribbleando en dirección al aro, salgo y caigo con los dos pies juntos, y tiro al aro. Puede ser de frente o con cambio de dirección", "15 hop steps" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("Gancho con giro","Me paro de espaldas al aro sobre la linea de la pintura. Me doy vuelta hacía afuera,hago un pique y tiro un gancho. Me concentro en dar los pasos correctos y en que la pelota no baje de la altura del hombro", "2 series de 12 ganchos por lado" , R.drawable.placeholdergif))
                 exercisesList.add(DataModel("Flotadora simple","Empiezo dribleando desde fuera de la pintura, entro en diagonal y defino con una flotadora. Me concentro en dar los pasos correctos, en extenderme completamente y en mantener la pose después de tirar", "15 flotadoras por lado." , R.drawable.placeholdergif))
             }
         }
--- app/src/main/java/com/citesoftware/basker/categorias/PostActivity.kt
+++ app/src/main/java/com/citesoftware/basker/categorias/PostActivity.kt
@@ -23,22 +23,24 @@ class PostActivity : AppCompatActivity() {
         when(intent.getIntExtra("dia", 1)){
 
             1 -> {
+
                 exercisesList.add(DataModel("Giro de poste basico","Me paro de dribbleando de espaldas al aro (posición de poste) dentro de la pintura. Me acerco al aro, y con pie de pivot hago una bandeja", "10 tiros por lado" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Gancho fijo","Me paro enfrente del aro, en paralelo al tablero. Tiro un gancho con el brazo más alejado del aro concentrandome en hacer un giro amplio", "12 ganchos por lado" , R.drawable.placeholdergif))
                 exercisesList.add(DataModel("Post fadeaway","En posición de poste, me acerco al aro, me doy vuelta y tiro a distancia", "5 tiros encestados desde 4 lugares distintos" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("Pump fake con paso","En posición de poste, me acerco al aro, me doy vuelta y hago un amague de tiro, y con el pie de pivot doy un paso para hacer una bandeja hacía el otro lado", "10 por lado" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Putbacks","Tiro la pelota contra el tablero para simular un tiro fallado, agarro el rebote e instantaneamente tiro. Idealmente llego a tirar antes de tocar el piso.", "2 series de 10 tiros" , R.drawable.placeholdergif))
+
             }
             2 -> {
-                exercisesList.add(DataModel("Rondo","Corro hacía el aro como para hacer una bandeja, pero freno el movimiento extendiendo el brazo antes de tirar, y me doy vuelta para tirar", "10 por lado" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Mikan Drill","Desde abajo del aro, doy un paso con la pierna izquierda hacía la derecha y en el mismo movimiento con hago una bandeja de mano derecha. Agarro la pelota apenas puedo y hago el movimiento inverso hacía la izquierda, y así sucesivamente.", "3 series de 10 bandejas seguidas" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Gancho simple","Entro a la pintura en paralelo al tablero y tiro un gancho. Me concentro en dar los pasos correctos y en que la pelota no baje de la altura del hombro.", "12 ganchos por lado" , R.drawable.placeholdergif))
                 exercisesList.add(DataModel("Hesi fadeaway","En posición de poste dentro de la pintura, finto para un lado, para el otro, y me doy vuelta para tirar a distancia. Puedo hacerlo con salto en diagonal hacía atrás (fadeaway)", "5 tiros encestados desde 4 lugares distintos" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Up and under","En posición de poste, me acerco al aro, me doy vuelta y hago un amague de tiro, y con el pie de pivot doy un paso para hacer una bandeja hacía el otro lado", "10 por lado" , R.drawable.placeholdergif))
             }
             3 -> {
-                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
-                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Rondo","Corro hacía el aro como para hacer una bandeja, pero freno el movimiento extendiendo el brazo antes de tirar, y me doy vuelta para tirar", "10 por lado" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Gancho con giro","Me paro de espaldas al aro sobre la linea de la pintura. Me doy vuelta hacía afuera,hago un pique y tiro un gancho. Me concentro en dar los pasos correctos y en que la pelota no baje de la altura del hombro", "2 series de 12 ganchos por lado" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Putback Up and under ","Tiro la pelota contra el tablero para simular un tiro fallado, agarro el rebote, hago un amague de tiro, y con el pie de pivot doy un paso para hacer una bandeja hacía el otro lado", "2 series de 12 tiros" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Spin fadeaway","En posición de poste, giro encarando hacía el aro, doy el primer paso como en un eurostep, pero cambio de dirección hacía atrás en el segundo y tiro alejandome del aro", "15 tiros encestados" , R.drawable.placeholdergif))
             }
         }
 
