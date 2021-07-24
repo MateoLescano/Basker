a3104e6171cd751374afc0aac71a873968c7da3f
MateoLescano <85969608+MateoLescano@users.noreply.github.com>
2021-07-24 11:46:27 -0300

    20-7

--- app/src/main/java/com/citesoftware/basker/categorias/PostActivity.kt
+++ app/src/main/java/com/citesoftware/basker/categorias/PostActivity.kt
@@ -23,13 +23,22 @@ class PostActivity : AppCompatActivity() {
         when(intent.getIntExtra("dia", 1)){
 
             1 -> {
-
+                exercisesList.add(DataModel("Giro de poste basico","Me paro de dribbleando de espaldas al aro (posición de poste) dentro de la pintura. Me acerco al aro, y con pie de pivot hago una bandeja", "10 tiros por lado" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Post fadeaway","En posición de poste, me acerco al aro, me doy vuelta y tiro a distancia", "5 tiros encestados desde 4 lugares distintos" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Pump fake con paso","En posición de poste, me acerco al aro, me doy vuelta y hago un amague de tiro, y con el pie de pivot doy un paso para hacer una bandeja hacía el otro lado", "10 por lado" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
             }
             2 -> {
-
+                exercisesList.add(DataModel("Rondo","Corro hacía el aro como para hacer una bandeja, pero freno el movimiento extendiendo el brazo antes de tirar, y me doy vuelta para tirar", "10 por lado" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("Hesi fadeaway","En posición de poste dentro de la pintura, finto para un lado, para el otro, y me doy vuelta para tirar a distancia. Puedo hacerlo con salto en diagonal hacía atrás (fadeaway)", "5 tiros encestados desde 4 lugares distintos" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
             }
             3 -> {
-
+                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
+                exercisesList.add(DataModel("","", "" , R.drawable.placeholdergif))
             }
         }
 
