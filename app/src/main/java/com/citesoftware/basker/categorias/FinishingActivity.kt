5605d6af75dd2dcf855967732e636cdff379cb1a
MateoLescano <mateolescano@gmail.com>
2022-03-03 14:31:13 -0300

    Plugin, Logo y Finishing update

--- app/src/main/java/com/citesoftware/basker/categorias/FinishingActivity.kt
+++ app/src/main/java/com/citesoftware/basker/categorias/FinishingActivity.kt
@@ -25,17 +25,20 @@ class FinishingActivity : AppCompatActivity() {
             1 -> {
                 exercisesList.add(DataModel(getString(R.string.Tex35),getString(R.string.Dex35), getString(R.string.Rex35) , R.drawable.giffin1d1))
                 exercisesList.add(DataModel(getString(R.string.Tex36),getString(R.string.Dex36), getString(R.string.Rex36) , R.drawable.giffin1d2))
+                exercisesList.add(DataModel(getString(R.string.Tex44),getString(R.string.Dex44), getString(R.string.Rex35) , R.drawable.placeholdergif))
                 exercisesList.add(DataModel(getString(R.string.Tex37),getString(R.string.Dex37), getString(R.string.Rex37) , R.drawable.giffin1d3))
             }
             2 -> {
                 exercisesList.add(DataModel(getString(R.string.Tex38),getString(R.string.Dex38), getString(R.string.Rex38) , R.drawable.placeholdergif))
-                exercisesList.add(DataModel(getString(R.string.Tex39),getString(R.string.Dex39), getString(R.string.Rex39) , R.drawable.placeholdergif))
                 exercisesList.add(DataModel(getString(R.string.Tex40),getString(R.string.Dex40), getString(R.string.Rex40) , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex45),getString(R.string.Dex45), getString(R.string.Rex37) , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex46),getString(R.string.Dex46), getString(R.string.Rex37) , R.drawable.placeholdergif))
             }
             3 -> {
                 exercisesList.add(DataModel(getString(R.string.Tex41),getString(R.string.Dex41), getString(R.string.Rex41) , R.drawable.placeholdergif))
                 exercisesList.add(DataModel(getString(R.string.Tex42),getString(R.string.Dex42), getString(R.string.Rex42) , R.drawable.placeholdergif))
                 exercisesList.add(DataModel(getString(R.string.Tex43),getString(R.string.Dex43), getString(R.string.Rex43) , R.drawable.placeholdergif))
+                exercisesList.add(DataModel(getString(R.string.Tex39),getString(R.string.Dex39), getString(R.string.Rex39) , R.drawable.placeholdergif))
             }
         }
 
